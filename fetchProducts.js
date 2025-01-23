582eed9cbeed89cff701a0d46b2fd3f0273adcf1
dmntrx <145675897+dmntrx@users.noreply.github.com>
2025-01-24 00:17:07 +0300

    изменение обращения к json
    локально -> сервер

--- fetchProducts.js
+++ fetchProducts.js
@@ -1,6 +1,6 @@
 export async function fetchProducts() {
     try {
-        const response = await fetch('products.json');
+        const response = await fetch('https://dmntrx.github.io/webdev-exam-2024-1-shkuran/products.json');
         if (!response.ok) throw new Error('Failed to load products data');
         return await response.json();
     } catch (error) {
